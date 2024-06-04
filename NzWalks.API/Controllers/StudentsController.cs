e3dc5e43becbff62db8c252429ba785e98843a28
Selim BAYINDIR <byndr28@gmail.com>
2024-06-04 20:21:11 +0300

    Add project files.

---
+++ NzWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NzWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class StudentsController : Controller
+    {
+       
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentsNames = new string[] { "Ali", "Veli", "Nazım","Hikmet" };
+            return Ok(studentsNames);
+        }
+    }
+}
+
---
+++ NzWalks.API/Models/Domain/Difficulty.cs
@@ -0,0 +1,9 @@
+﻿namespace NzWalks.API.Models.Domain
+{
+    public class Difficulty
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
